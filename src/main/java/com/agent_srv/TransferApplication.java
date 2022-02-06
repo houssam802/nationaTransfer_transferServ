ff00369bd0203d8b01cbb4954fc7dd80067431a5
Houssam El aich <elaich.houssam@gmail.com>
2022-02-06 11:33:42 +0100

    eureka client

--- src/main/java/com/agent_srv/TransferApplication.java
+++ src/main/java/com/agent_srv/TransferApplication.java
@@ -2,10 +2,12 @@ package com.agent_srv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TransferApplication {
 
     public static void main(String[] args) {
